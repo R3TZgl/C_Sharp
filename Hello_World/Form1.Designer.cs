537b13efd4df5174996f2f131e7190c7ac8eca12
Gabriel Retzlaff <gretzlaff13@gmail.com>
2022-06-23 22:16:27 -0300

    Aula 1

--- Hello_World/Form1.Designer.cs
+++ Hello_World/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnClique = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClique
+            // 
+            this.btnClique.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClique.Location = new System.Drawing.Point(368, 187);
+            this.btnClique.Name = "btnClique";
+            this.btnClique.Size = new System.Drawing.Size(74, 40);
+            this.btnClique.TabIndex = 0;
+            this.btnClique.Text = "CLIQUE";
+            this.btnClique.UseVisualStyleBackColor = true;
+            this.btnClique.Click += new System.EventHandler(this.btnClique_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Formulário";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(748, 354);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClique);
+            this.Name = "Form1";
+            this.Text = "Meu Primeiro Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnClique;
+        private Label label1;
     }
 }
--- Hello_World/Form1.cs
+++ Hello_World/Form1.cs
@@ -6,5 +6,15 @@ namespace Hello_World
         {
             InitializeComponent();
         }
+
+        private void btnClique_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+
+            Close(); //Fechar uma janela
+            Application.Exit(); //Fechar o programa
+
+        }
+
     }
 }
