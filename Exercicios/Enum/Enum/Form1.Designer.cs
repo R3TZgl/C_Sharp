da452f0c1204c188e64199ff0aef5b4f7e2c9ee0
Gabriel Retzlaff <gretzlaff13@gmail.com>
2022-08-30 22:03:43 -0300

    asp

---
+++ Exercicios/Enum/Enum/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Enum
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lblEnum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(361, 324);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(136, 50);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "MOSTAR ENUM";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lblEnum
+            // 
+            this.lblEnum.AutoSize = true;
+            this.lblEnum.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblEnum.Location = new System.Drawing.Point(53, 168);
+            this.lblEnum.Name = "lblEnum";
+            this.lblEnum.Size = new System.Drawing.Size(57, 21);
+            this.lblEnum.TabIndex = 2;
+            this.lblEnum.Text = "Enum: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEnum);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnMostrar;
+        private Label lblEnum;
+    }
+}
---
+++ Exercicios/Enum/Enum/Form1.cs
@@ -0,0 +1,25 @@
+namespace Enum
+{
+    public partial class Form1 : Form
+    {
+        enum DiasSemana
+        {
+            Segunda,
+            Terca,
+            Quarta,
+            Quinta,
+            Sexta,
+            Sabado,
+            Domingo
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
